8196f8c88f4dbd13c36bdfd176f68351f693ab3d
Guilherme Seleri <110575996+guilhermesealves@users.noreply.github.com>
2023-03-02 20:21:13 -0300

    Finalizando a atividade 02

--- index02.php
+++ index02.php
@@ -10,14 +10,20 @@
 </head>
 
 <body>
-    <h1>Cadastro novo usuário</h1>
-    <div class="continer mt-5">
+    <div class="continer mt-5 ms-5">
+        <div class="header-title mt-2">
+        <h1>Cadastro novo usuário</h1>
+        </div>
+        
         <div class="col">
             <div class="row">
                 <div class="col-md-4 ml-auto">
                     <form action="" method="post">
                         <div class="form-group">
                             <input type="text" name="nome" class="form-control" placeholder="Nome">
+                        </div>
+                        <div class="form-group mt-1 ">
+                            <input type="text" name="sobrenome" class="form-control" placeholder="Sobrenome">
                         </div>
                         <div class="form-group mt-1">
                             <input type="email" name="email" class="form-control" placeholder="Email">
@@ -38,13 +44,23 @@
                             <div class="col-md-6 ml-auto">
                                 <form action="" method="post">
                                     <div class="form-group">
-                                        <input type="text" name="nome" class="form-control" placeholder="Nome">
+                                        <input type="password" name="senha" class="form-control" placeholder="Senha">
                                     </div>
                                     <div class="form-group mt-1">
-                                        <input type="email" name="email" class="form-control" placeholder="Email">
+                                        <input type="password" name="email" class="form-control" placeholder="Confirmar Senha">
+                                    </div>
+                                    <div class="mt-1">
+                                        <select class="form-select">
+                                            <option selected>Selecione uma pergunta de segurança</option>
+                                            <option >Qual estação do ano você nasceu</option>
+                                            <option >Qual numero de telefone antigo</option>
+                                            <option >Qual o nome do seu PET de extimação</option>
+
+
+                                        </select>
                                     </div>
                                     <div class="form-group mt-1">
-                                        <input type="number" name="phone" class="form-control" placeholder="Phone">
+                                        <input type="text" name="resposta" class="form-control" placeholder="Resposta">
                                     </div>
                                     <div class=" col-md-5 mt-3">
                                         <button type="submit" class="btn btn-primary w-100">Cadastr-se agora!</button>
